61265408c409ef4209690885d510214754600c90
Michael <Michael@DESKTOP-MEFV97J.mshome.net>
2018-09-15 02:18:04 +0200

    Version 0.0.2 mit J3 support

--- com_helloworld/site/helloworld.php
+++ com_helloworld/site/helloworld.php
@@ -1,15 +1,21 @@
 <?php
-// No direct access to this file
-defined('_JEXEC') or die;
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-jimport('joomla.application.component.controller');
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
 
 // Get an instance of the controller prefixed by HelloWorld
-$controller = JController::getInstance('HelloWorld');
+$controller = JControllerLegacy::getInstance('HelloWorld');
 
 // Perform the Request task
-$controller->execute(JRequest::getCmd('task'));
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
 
 // Redirect if set by the controller
 $controller->redirect();
-?>
